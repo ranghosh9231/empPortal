b0efa98b4fd800e17cbc54e7ab541383584fcf67
Ranjan Ghosh <ranjanghosh9231@gmail.com>
2019-06-22 12:48:44 +0530

    Initial Commit

---
+++ src/main/java/com/emp/EmpPortalApplication.java
@@ -0,0 +1,14 @@
+package com.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpPortalApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO EMPLOYEE (first_name, last_name, gender, date_of_birth, department) VALUES
+  ('Manish', 'Pandey', 'M', '10/09/89', 'Middle Order'),
+  ('Mithali', 'Raj', 'F', '03/12/82', 'Opener'),
+  ('Murali', 'Vijay', 'M', '01/04/84', 'Opener'),
+  ('Harmanpreet', 'Kaur', 'F', '08/03/89', 'Allrounder '),
+  ('Mayank', 'Agarwal', 'M', '16/02/91', 'Opener');
